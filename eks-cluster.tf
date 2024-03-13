4d19fdc1b811864c7c8d7e3bd9f1708307ba7b30
will <contatowilliamsj@gmail.com>
2024-03-13 14:45:23 -0300

    feat: base file with only necessary resources

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -18,15 +18,14 @@ module "eks" {
 
   # TODO add app configuration pods
   eks_managed_node_groups = {
-    # Example:
-    # one = {
-    #   name = "node-group-1"
+    one = {
+      name = "node-group-1"
 
-    #   instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
-    #   min_size     = 1
-    #   max_size     = 3
-    #   desired_size = 2
-    # }
+      min_size     = 1
+      max_size     = 5
+      desired_size = 3
+    }
   }
 }
