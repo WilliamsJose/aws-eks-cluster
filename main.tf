2b1ad0ab5427b298436de454f0348eda3c55568a
will <contatowilliamsj@gmail.com>
2024-03-13 12:14:18 -0300

    BREAKING CHANGE: add some tags and comment eks node groups, remove vpc from main to file

--- main.tf
+++ main.tf
@@ -14,37 +14,6 @@ resource "random_string" "suffix" {
   special = false
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = data.aws_availability_zones.available.names
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
@@ -56,35 +25,29 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+  # TODO add github info
+  tags = {
+    Environment = "Development"
+    GithubRepo  = "YourRepoNameHere"
+    GithubOrg   = "YourOrganizationGitNameHere"
   }
 
+  # TODO add app configuration pods
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+    # Example:
+    # one = {
+    #   name = "node-group-1"
 
-      instance_types = ["t3.small"]
+    #   instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
+    #   min_size     = 1
+    #   max_size     = 3
+    #   desired_size = 2
+    # }
   }
 }
 
-
+# Terraform default:
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
