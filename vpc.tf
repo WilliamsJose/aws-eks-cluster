1ca56786ac31e63030ae0303f80d9e7721de6a4b
will <contatowilliamsj@gmail.com>
2024-03-17 20:37:49 -0300

    chore: remove comments

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,3 @@
-# locals {
-#   newbits         = 8
-#   netcount        = 6
-#   all_subnets     = [for i in range(local.netcount) : cidrsubnet(var.vpc_cidr, local.newbits, i)]
-#   public_subnets  = slice(local.all_subnets, 0, 3)
-#   private_subnets = slice(local.all_subnets, 3, 6)
-# }
 
 resource "aws_default_vpc" "default" {}
 
@@ -31,38 +24,3 @@ resource "aws_default_subnet" "default_az2_private" {
     Name = "Default subnet for us-east-1c"
   }
 }
-
-# module "vpc" {
-#   # public vpc module
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.0.0"
-
-#   # vpc name
-#   name   = var.vpc_name
-#   vpc_id = aws_default_vpc.default.id
-
-#   # cidr = var.vpc_cidr
-#   # availability zones
-#   # azs = slice(data.aws_availability_zones.available.names, 0, 3)
-
-#   # private_subnets = local.private_subnets
-#   # public_subnets  = local.public_subnets
-
-#   enable_nat_gateway   = var.enable_nat_gateway
-#   single_nat_gateway   = var.single_nat_gateway
-#   enable_dns_hostnames = var.enable_dns_hostnames
-
-#   tags = {
-#     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-#   }
-
-#   public_subnet_tags = {
-#     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-#     "kubernetes.io/role/elb"                    = 1
-#   }
-
-#   private_subnet_tags = {
-#     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-#     "kubernetes.io/role/internal-elb"           = 1
-#   }
-# }
